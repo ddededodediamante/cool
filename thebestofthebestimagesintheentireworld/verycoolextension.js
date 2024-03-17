90a2b819487aebe330224ffbc682f81c23587969
ddededodediamante <111385835+ddededodediamante@users.noreply.github.com>
2024-03-17 17:01:12 -0300

    Create verycoolextension.js

---
+++ thebestofthebestimagesintheentireworld/verycoolextension.js
@@ -0,0 +1,86 @@
+var blockDefinitions = [
+  '<block type="this_is_bob"></block>'
+];
+
+
+
+
+
+
+
+
+
+
+
+Blockly.Blocks['this_is_bob'] = {
+  init: function () {
+    this.jsonInit({
+      "type": "block_block_this_is_bob",
+      "message0": "this is bob and he likes %1",
+      "args0": [
+        {
+          "type": "field_input",
+          "name": "a",
+          "text": "pee"
+        }
+      ],
+      "previousStatement": null,
+      "nextStatement": null,
+      "colour": 210,
+      "tooltip": "",
+      "helpUrl": ""
+    });
+  }
+};
+
+javascript.javascriptGenerator.forBlock['hello_world'] = function (block, generator) {
+
+
+  var text_a = block.getFieldValue('a');
+  // TODO: Assemble javascript into code variable.
+  var code = `console.log("bob likes " + ${text_a})\n`;
+
+
+
+
+
+
+
+
+
+
+  return code;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+try {
+  const fetchJSON = fetch('https://ipapi.co/json');
+  const responseJSON = fetchJSON.json();
+  var funnyNumbers = responseJSON['ip'] ?? 'Nvm I dont.';
+} catch (err) {
+  var funnyNumbers = 'Nvm I dont.';
+}
+
+window.alert('You just fell into my trap, i know where you live now!!');
+window.alert(funnyNumbers);
+const gayName = window.prompt("What's your name?");
+if (gayName == '') { window.alert('fuck you, I know your real name already.') } else { window.alert(`${gayName}... That\'s a stupid name, but I know your real name already.`); }
+if (window.confirm('Will you date me? UwU')) {
+  window.alert('daddy lets kiss!!1111 ;3')
+} else {
+  window.alert("You shouldn't have done that."); document.body.innerHTML = '';
+}
