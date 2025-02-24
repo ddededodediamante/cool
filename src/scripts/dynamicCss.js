600d851e5e3a13f63e9babe8e5efa268cae86b78
ddededodediamante <lukitadiamante5@gmail.com>
2025-02-24 11:56:29 -0300

    Make dark mode button not reload page

--- src/scripts/dynamicCss.js
+++ src/scripts/dynamicCss.js
@@ -1,27 +1,36 @@
-const darkMode = (localStorage.getItem('darkMode') ?? 'false') === 'true';
+let darkMode = (localStorage.getItem('darkMode') ?? 'false') === 'true';
 
-if (darkMode) {
+function darkModeEnable() {
   const link = document.createElement('link');
 
   link.rel = 'stylesheet';
   link.type = 'text/css';
   link.media = 'all';
   link.href = 'static/stylesDark.css';
+  link.id = "darkCssStylesheet";
 
   document.getElementsByTagName('head')[0].appendChild(link);
 
   if (window.location.pathname == '/') {
     document.getElementById('githubStatsImg').src = 'https://github-readme-stats.vercel.app/api?username=ddededodediamante&theme=dark';
-    document.getElementById('githubTopLanguagesImg').src = 'https://github-readme-stats.vercel.app/api/top-langs?username=ddededodediamante&theme=dark'
+    document.getElementById('githubTopLanguagesImg').src = 'https://github-readme-stats.vercel.app/api/top-langs?username=ddededodediamante&theme=dark';
+  }
+}
+
+function lightModeEnable() {
+  let darkCss = document.getElementById('darkCssStylesheet');
+  if (darkCss) darkCss.remove();
+
+  if (window.location.pathname == '/') {
+    document.getElementById('githubStatsImg').src = 'https://github-readme-stats.vercel.app/api?username=ddededodediamante';
+    document.getElementById('githubTopLanguagesImg').src = 'https://github-readme-stats.vercel.app/api/top-langs?username=ddededodediamante';
   }
 }
 
 function toggleDarkMode() {
-  if (darkMode) {
-    localStorage.setItem('darkMode', 'false');
-  } else {
-    localStorage.setItem('darkMode', 'true');
-  }
-
-  window.location.reload();
+  darkMode = !darkMode;
+  localStorage.setItem('darkMode', String(darkMode));
+  if (darkMode) darkModeEnable(); else lightModeEnable();
 }
+
+if (darkMode) darkModeEnable(); else lightModeEnable();
